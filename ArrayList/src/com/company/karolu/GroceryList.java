f8b09e9c422acef484a88a2594943a9795699cba
karoludevs <karoludevs@gmail.com>
2016-06-13 12:36:45 +0200

    GroceryList class almost done

--- ArrayList/src/com/company/karolu/GroceryList.java
+++ ArrayList/src/com/company/karolu/GroceryList.java
@@ -13,7 +13,32 @@ public class GroceryList {
         System.out.println("You have added an item.");
     }
 
-    public void showGroseryList(){
-        groceryList.get()
+    public void printGroseryList() {
+            if (groceryList.contains(0)) {
+                System.out.println("The list is empty");
+
+            } else {
+                for (int i = 0; i < groceryList.size(); i++) {
+                    System.out.println(i + 1 + ". " + groceryList.get(i) + "\n");
+
+            }
+        }
     }
-}
+
+    public String  findGroceryItem(String item){
+            int position = groceryList.indexOf(item);
+        if(position >= 0){
+            return groceryList.get(position);
+            }else{
+            return null;
+        }
+    }
+
+    public String modifyGroceryItem(int position, String newItem){
+        groceryList.set(position, newItem);
+        return newItem;
+    }
+
+    }
+
+
